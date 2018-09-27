39ac41d87e49e425cc8a5c3f55bf988e50ff12e9
SAL\Harley <harleyrunner13@gmail.com>
2018-09-26 17:07:28 -0700

    Add remaining analysis scripts created to date

---
+++ sample/con_ops.py
@@ -0,0 +1,119 @@
+# Utilized Modules
+import numpy as np
+import Requirements as Req
+from parts_list import Parts_List
+
+from Operational_Analyses import State_Determine
+from Operational_Analyses import Power_Consume, Total_Volume, Total_Mass
+
+from Orbital_Analyses import u
+
+# Spacecraft State: lists operational conditions of subsystems/components
+
+
+def bool_swap(state):
+    # Change the state of a component into a boolean-ish value
+    global boolish
+    if state == 'on':
+        boolish = 1.0
+    if state == 'idle':
+        boolish = 0.5
+    if state == 'off':
+        boolish = 0.0
+
+    return boolish
+
+
+def component_operations(rad, vel, jd, sun, GS, target, gs_inert):
+    # TODO: Add check for array/matrix input size, return error if not valid
+    sc_state = {}
+    sc_state['solar_panel_mode'] = np.asmatrix(np.zeros((1, Req.Steps),
+                                               np.dtype(object)))
+    sc_state['radio_mode'] = np.asmatrix(np.zeros((1, Req.Steps),
+                                         np.dtype(object)))
+    sc_state['antenna_mode'] = np.asmatrix(np.zeros((1, Req.Steps),
+                                           np.dtype(object)))
+    sc_state['magnetorquer_mode'] = np.asmatrix(np.zeros((1, Req.Steps),
+                                                np.dtype(object)))
+    sc_state['payload_mode'] = np.asmatrix(np.zeros((1, Req.Steps),
+                                           np.dtype(object)))
+    sc_state['processor_mode'] = np.asmatrix(np.zeros((1, Req.Steps),
+                                             np.dtype(object)))
+    sc_state['eps_mode'] = np.asmatrix(np.zeros((1, Req.Steps),
+                                       np.dtype(object)))
+    sc_state['batt_capacity'] = np.asmatrix(np.zeros((1, Req.Steps),
+                                            np.dtype(object)))
+    sc_state['data_capacity'] = np.asmatrix(np.zeros((1, Req.Steps),
+                                            np.dtype(object)))
+    sc_state['time'] = jd
+
+    # Initialize looping variables
+    batt_capacity = Req.Batt_max_capacity
+    batt_per = np.asmatrix(np.zeros((Req.Steps, 1), dtype=float))  # <-- get rid of eventually
+    data_capacity = Parts_List['Payload']['storage'] + Parts_List['Processor']['storage']
+    data_stored = 0.0 * u.byte
+    data_per = np.asmatrix(np.zeros((Req.Steps, 1), dtype=float))  # <-- get rid of eventually
+
+    for i in range(Req.Steps):
+        # Determine the state of each component at each step
+        # Based on orbital analysis data and component dependencies
+        sc_state = State_Determine(sc_state, batt_capacity,
+                                   Req.Batt_max_capacity, Req.Batt_min_percent,
+                                   i, sun, GS, target, data_stored)
+
+        # Power consumtion of the system:
+        # Adds up all the power consumption and input and updates current value
+        batt_capacity, batt_per[i, 0] = Power_Consume(sc_state, batt_capacity,
+                                                      Req.Batt_max_capacity, i)
+        sc_state['batt_capacity'][0, i] = batt_capacity
+
+        # TODO: Link Budget
+        data_uplink = 115.2 * u.kbyte / u.s  # bps - max rate
+        data_downlink = 100.0 * u.kbyte / u.s  # bps - max rate
+
+        # Set production of data from camera
+        if sc_state['payload_mode'][0, i] != 'off':
+            data_stored = data_stored + Parts_List['Payload']['data_picture']
+        # Check if data_stored exceeds maximum storage
+        if data_stored > data_capacity:
+            data_stored = data_stored * 0.75  # delete 25% of data (CHANGE LATER)
+        # Uplink / Downlink
+        if sc_state['antenna_mode'][0, i] != 'off':
+            # Downlink
+            data_stored = (data_stored.to(u.byte) -
+                           (data_downlink.to(u.byte / u.sec) * (60 * u.s)))
+            # TODO: Uplink (currently incorrect)
+#            data_stored = data_stored + (data_uplink * (60 * u.s))
+        if data_stored.magnitude < 0.0:
+            data_stored = (0.0 * u.byte)
+        data_per[i, 0] = ((data_stored / data_capacity).to(u.byte)).magnitude
+        sc_state['data_capacity'][0, i] = data_capacity
+
+    # Component Volume Analysis
+    sc_vol = Total_Volume()
+
+    # Component Mass Analysis
+    sc_mass = Total_Mass()
+
+    # Set state data into "Boolean" for visalization
+    boo = np.asmatrix(np.zeros((Req.Steps, 7), np.dtype(float)))
+    for j in range(Req.Steps):
+        boo[j, 0] = bool_swap(sc_state['eps_mode'][0, j])
+        boo[j, 1] = bool_swap(sc_state['payload_mode'][0, j])
+        boo[j, 2] = bool_swap(sc_state['radio_mode'][0, j])
+        boo[j, 3] = bool_swap(sc_state['antenna_mode'][0, j])
+        boo[j, 4] = bool_swap(sc_state['magnetorquer_mode'][0, j])
+        boo[j, 5] = bool_swap(sc_state['processor_mode'][0, j])
+        boo[j, 6] = bool_swap(sc_state['solar_panel_mode'][0, j])
+
+    return sc_state, batt_per, data_per, boo, sc_vol, sc_mass
+
+
+
+
+
+
+
+
+
+
---
+++ sample/main.py
@@ -0,0 +1,89 @@
+# Utilized Modules
+import numpy as np
+import time
+
+from Orbital_Analyses.Propagation import Prop_Cowell
+
+from Orbital_Analyses.Transform_Coordinate import Geodetic2ECFixed
+from Orbital_Analyses.Transform_Coordinate import FK5_J20002ECFixed
+from Orbital_Analyses.Transform_Coordinate import FK5_ECFixed2J2000
+from Orbital_Analyses.Transform_Coordinate import ECFixed2Geodetic
+
+from Orbital_Analyses.GS_Contact_Times import satContact
+from Orbital_Analyses.Sun_Contact_Times import satIlluminated
+from Orbital_Analyses.Geo_Contact_Times import satOverhead
+
+from con_ops import component_operations
+
+import Requirements as Req
+# from Orbital_Analyses import u
+
+t0 = time.time()
+# %%  Orbit Propagation & Analysis
+
+# Initialize matrices to hold calculated rad, vel, JD values
+orbit_r = np.asmatrix(np.zeros((3, Req.Steps), dtype=float))
+orbit_v = np.asmatrix(np.zeros((3, Req.Steps), dtype=float))
+orbit_jd = np.asmatrix(np.zeros((1, Req.Steps), dtype=float))
+orbit_gd = np.asmatrix(np.zeros((6, Req.Steps), dtype=float))
+
+# Propagate Orbit by defined number of steps using Cowell's Method
+[orbit_r, orbit_v, orbit_jd, orbit_gd] = Prop_Cowell(Req.Rad, Req.Vel,
+                                                     Req.GD_UTC, Req.Steps,
+                                                     Req.Ssize)
+
+# Illumination Simulation
+# INPUT: Inertial
+inSun = np.asmatrix(np.zeros((1, Req.Steps), dtype=np.float64))
+for i in range(Req.Steps):
+    inSun[:, i] = satIlluminated(orbit_r[:, i], orbit_gd[:, i])
+
+# Convert Ground Station to fixed
+gs_zero = np.asmatrix(np.zeros((3, 1), dtype=np.float64))
+gs_geo = np.matrix([[Req.gs_ll[0, 0].magnitude],
+                    [Req.gs_ll[1, 0].magnitude],
+                    [Req.gs_a.magnitude]])
+gs_fix = Geodetic2ECFixed(gs_geo)
+
+# Ground Station Contact Simulation
+# INPUT: Inertial
+inContact = np.asmatrix(np.zeros((1, Req.Steps), dtype=int))
+for j in range(Req.Steps):
+    # Convert ground station to inertial
+    gs_inert, gs_vel = FK5_ECFixed2J2000(gs_fix, gs_zero, orbit_gd[:, j])
+    inContact[:, j] = satContact(orbit_r[:, j], gs_inert)
+
+# Transform from J2000 to Fixed Frame (EFEC)
+# INPUT: Inertial, OUTPUT: Fixed
+fixed_r = np.asmatrix(np.zeros((3, Req.Steps), dtype=np.float64))
+fixed_v = np.asmatrix(np.zeros((3, Req.Steps), dtype=np.float64))
+for i in range(Req.Steps):
+    fixed_r[:, i], fixed_v[:, i] = FK5_J20002ECFixed(orbit_r[:, i],
+                                                     orbit_v[:, i],
+                                                     orbit_gd[:, i])
+
+# Calculate Geodetic Coordinates of the satellite (Lat / Lon / Alt)
+# INPUT: Fixed, OUTPUT: Geodetic
+sat_geo = np.asmatrix(np.zeros((3, Req.Steps), dtype=int))
+for j in range(Req.Steps):
+    sat_geo[:, j] = ECFixed2Geodetic(fixed_r[:, j])
+
+# Calculate if Sat is over CA
+# INPUT: Geodetic
+inCA = np.asmatrix(np.zeros((1, Req.Steps), dtype=int))
+for k in range(Req.Steps):
+    inCA[:, k] = satOverhead(sat_geo[:, k])
+
+# Run Con-Ops
+# Convert Ground Station to Inertial coordinates across steps
+gs_zero = np.asmatrix(np.zeros((3, Req.Steps), dtype=np.float64))
+sc_zero = np.asmatrix(np.zeros((3, 1), dtype=np.float64))
+gs_inert = np.asmatrix(np.zeros((3, Req.Steps), dtype=np.float64))
+for l in range(Req.Steps):
+    gs_inert[:, l], gs_zero[:, l] = FK5_ECFixed2J2000(gs_fix, sc_zero,
+                                                      orbit_gd[:, l])
+
+sc_state, sc_batt_percent, sc_data_percent, boo, sc_vol, sc_mass = component_operations(fixed_r, fixed_v, orbit_jd, inSun, inContact, inCA, gs_inert)
+
+t1 = time.time()
+print(t1 - t0)
