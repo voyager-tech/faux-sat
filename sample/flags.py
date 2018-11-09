05c76e766d5820fc9c75e87373a8738b3f3aed53
SAL\Harley <harleyrunner13@gmail.com>
2018-11-09 09:02:17 -0800

    Flag classes to be used along with the State Machine & Functional Analyses

---
+++ sample/flags.py
@@ -0,0 +1,27 @@
+# Defines types of flags that can be instanciated by the state machine
+
+# TODO: Add DocStrings to all classes
+# TODO: Possible combine into one class with variable arguments
+
+
+# Flag (basic - only represents if a function should be run)
+class FlagBasic:
+    """ DocString here """
+    def __init__(self, boolean):
+        self.bool = boolean
+
+
+# Array Flag (refrences an array as an attribute of the flag)
+class FlagArray:
+
+    def __init__(self, boolean, array):
+        self.bool = boolean
+        self.array = array
+
+
+# Object Flag (Refrences an object as an attribute of the flag)
+class FlagObject:
+
+    def __init__(self, boolean, objects):
+        self.bool = boolean
+        self.obj = objects
