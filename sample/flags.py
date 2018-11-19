1119f036970798dd2c168af4b1e78548d4566718
SAL\Harley <harleyrunner13@gmail.com>
2018-11-19 11:11:28 -0800

    Condensed all flag classes into one flag type and added a return value field

--- sample/flags.py
+++ sample/flags.py
@@ -1,35 +1,10 @@
 # Defines types of flags that can be instanciated by the state machine
 
-# TODO: Add DocStrings to all classes
-# TODO: Possible combine into one class with variable arguments
 
-
-# Flag (basic - only represents if a function should be run)
-class FlagBasic:
-    """ DocString here """
-    def __init__(self, boolean):
-        self.bool = boolean
-
-
-# Array Flag (refrences a single number as an attribute of the flag)
+# Now can use any additional value with the boolean from just one class
 class FlagValue:
-
-    def __init__(self, boolean, value):
+    """ DocString here """
+    def __init__(self, boolean, input_value=None, return_value=None):
         self.bool = boolean
-        self.value = value
-
-
-# Array Flag (refrences an array as an attribute of the flag)
-class FlagArray:
-
-    def __init__(self, boolean, array):
-        self.bool = boolean
-        self.array = array
-
-
-# Object Flag (Refrences an object as an attribute of the flag)
-class FlagObject:
-
-    def __init__(self, boolean, objects):
-        self.bool = boolean
-        self.obj = objects
+        self.input_value = input_value
+        self.return_value = return_value
