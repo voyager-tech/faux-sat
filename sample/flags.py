d562a64ebe3a96ce5d18fba6578ebf460ea599ee
SAL\Harley <harleyrunner13@gmail.com>
2018-11-20 08:53:21 -0800

    Added kwargs to flag class to handle any additional data that might need to be stored in the flag instance

--- sample/flags.py
+++ sample/flags.py
@@ -4,7 +4,7 @@
 # Now can use any additional value with the boolean from just one class
 class FlagValue:
     """ DocString here """
-    def __init__(self, boolean, input_value=None, return_value=None):
+    def __init__(self, boolean, input_value=None, return_value=None, **kwargs):
         self.bool = boolean
         self.input_value = input_value
         self.return_value = return_value
