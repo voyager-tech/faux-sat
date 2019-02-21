f08876016ad74e661922dffe73a28e2a26f8f49e
SAL\Harley <harleyrunner13@gmail.com>
2019-02-20 22:51:16 -0800

    Beginning of script for determining planetary ephemerides

---
+++ sample/orbital_analyses/orbital_algorithms.py
@@ -0,0 +1,20 @@
+# Utilized Modules
+import numpy as np
+
+
+# Celestial Applications
+def planetary_ephemerides(planet, epoch):
+    # TODO: Add Docstring
+    '''
+    '''
+    # Determine if epoch is JD or GD and convert to GD
+
+    # convert_time function
+
+    # Values from tables
+
+    # KepEqtnE function
+
+    # COE2RV
+
+    # Covert to J2000
